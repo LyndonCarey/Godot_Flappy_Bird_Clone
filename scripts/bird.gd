778db93b8e434dd947c7c699340b3317b95a42ae
LyndonCarey <lyngaming170@gmail.com>
2026-02-15 01:26:23 -0500

    Flappy Bird V2
    UI Polish

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var GM:Node
 const SPEED = 300.0
 const JUMP_VELOCITY = -1100.0
+@onready var spawn_pos = position
+var time = 0
 
 @onready var anim = $AnimationPlayer
 @onready var srpite = $AnimatedSprite2D
@@ -22,15 +24,25 @@ func jump():
 	flap_sound.play()
 	if anim.current_animation != "Jump":
 		anim.play("Jump")
+	if GM.game_started == false:
+		GM.game_started = true
+		
 	
 
 func _input(event):
 	if (Input.is_action_just_pressed("Jump") or event is InputEventScreenTouch) and event.is_pressed() and is_alive:
 		jump()
+		
+func _process(delta):
+	time += 1*delta
+	if GM.game_started == false:
+		position = spawn_pos+Vector2.UP*32*sin(deg_to_rad(time*360))
+		
+	
 	
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and GM.game_started:
 		velocity += get_gravity() * delta
 		
 	if is_alive and velocity.y > 0 and anim.current_animation != "Fall":
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,7 +5,16 @@ extends Node
 
 @onready var score_label = $"../UI/Score"
 
+@onready var screen_ui = $"../UI/Message"
+
+@onready var spawn_timer = $"Spawn Timer"
 @onready var death_timer = $"Death Timer"
+
+var Bird_Scene = preload("uid://chm6nqk7egfpt")
+
+@onready var bird = Bird_Scene.instantiate()
+
+var Pipe_Scene = preload("uid://cthjs4u2fq3x0")
 
 @export var scroll_speed: float = 400
 @export var score: int = 0:
@@ -13,19 +22,21 @@ extends Node
 		if val > score: 
 			score = val
 			score_label.text = str(val)
+			
+@export var game_started = false:
+	set(val):
+		if val == true and game_over == false:
+			game_started = true
+			spawn_timer.start()
+			screen_ui.hide()
 		
-@export var game_over = false:
+var game_over = false:
 	set(val):
 		if val == true and game_over == false:
 			game_over = true
 			bird.is_alive = false
 			scroll_speed = 0
 			death_timer.start()
-
-var Bird_Scene = preload("uid://chm6nqk7egfpt")
-var Pipe_Scene = preload("uid://cthjs4u2fq3x0")
-
-@onready var bird = Bird_Scene.instantiate()
 	
 func _ready():
 	bird.GM = self
